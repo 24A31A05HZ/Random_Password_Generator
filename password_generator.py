2ed77713e1353d68b44ed032beb9926ab63c0ce1
Yaswanth Kumar Anusuri <yaswanthanusuri05@gmail.com>
2025-12-25 06:23:15 +0530

    Add password generator function with random characters

---
+++ password_generator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def generate_password(length=16):
+    upper = string.ascii_uppercase
+    lower = string.ascii_lowercase
+    digits = string.digits
+    special = string.punctuation
+
+    all_chars = upper + lower + digits + special
+
+    password = ''.join(random.choice(all_chars) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    print(generate_password())
